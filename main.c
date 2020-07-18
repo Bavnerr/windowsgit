f33a070a300a965e2a15f154f7352c8bb5eb8bbf
DeDe Ubuntu Lap <yarden984321@gmail.com>
2020-07-19 00:00:33 +0300

    re-write some stuff
    now you can't put X on O, removed some duplicate code
    AvnEr Ya GeveR
    
    Signed-off-by: DeDe Ubuntu Lap <yarden984321@gmail.com>

--- main.c
+++ main.c
@@ -1,14 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int drawBoard(char array[]);
-int markbaord(char array[],int turn);
-int checkForWin(char array[], int turn);
+int drawBoard(char array[]) {
 
-int main()
-{
-    int move;
-    int turn = 1;
+    printf("=====\n");
+    printf("%s",array);
+    printf("=====\n\n");
+}
+
+int checkForWin(char array[], int playerNum) {
+
+    if (array[0] == array[2] && array[0] == array[4]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[12] == array[14] && array[12] == array[16]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[24] == array[26] && array[24] == array[28]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[0] == array[12] && array[0] == array[24]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[2] == array[14] && array[2] == array[26]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[4] == array[16] && array[4] == array[28]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[0] == array[14] && array[0] == array[28]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+    if (array[4] == array[14] && array[4] == array[24]){
+        printf("\nPlayer %d Wins!!\n", playerNum);
+        return 1;
+    }
+}
+
+int play(char array[],int playerNum) {
+
+    int move, toReplace;
+    int strikes = 0;
+    int spots[9] = {0, 2, 4, 12, 14, 16, 24, 26 ,28};
+
+    smartAssReTrun:
+
+    drawBoard(array);
+    printf("Player %d type a location : ", playerNum);
+    scanf("%d",&move);
+    if (0 > move || move < 10) {
+        toReplace = spots[move - 1];
+        
+        if (playerNum == 2 && array[toReplace] != 'X')
+            array[toReplace] = 'O';
+
+        else if (playerNum == 1 && array[toReplace] != 'O')
+            array[toReplace] = 'X';
+
+        else if (strikes == 0) {
+            printf("\nYou MOTH*RFUCKER, you can't put it in here, choose other place\nAnd don't be a smart ass this time\n\n");
+            strikes++;
+            goto smartAssReTrun;
+        }
+
+        else if (strikes == 1) {
+            printf("\nHoo, come on, try me one more time\n\n");
+            strikes++;
+            goto smartAssReTrun;
+        }
+
+        else if (strikes == 2) {
+
+            char winnerChar = 'H';
+
+            if (playerNum == 1) {
+                winnerChar = 'X';
+                playerNum = 2;
+            }
+
+            else {
+                winnerChar = 'O';
+                playerNum = 1;
+            }
+
+            for (int i = 0; i < 9; i++)
+                array[spots[i]] = winnerChar;
+        }
+    }
+    else {
+        printf("\n Please choose number between 1-9 you MOTHERF*CKER\n\n");
+        goto smartAssReTrun;
+    }
+}
+
+int main() {
+
     printf("\tThis is a TicTacToe game:\n\tPlayer 1(X) Vs. Player 2(O)\n");
     static char board[30] = {
     '1','|','2','|','3','\n',
@@ -17,126 +110,20 @@ int main()
     '-','-','-','-','-','\n',
     '7','|','8','|','9','\n',
     };
-    drawBoard(board);
-    markbaord(board, turn);
-    turn++;
-    drawBoard(board);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    markbaord(board, turn);
-    turn--;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn++;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn--;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn++;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn--;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn++;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn--;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
-    turn++;
-    drawBoard(board);
-    markbaord(board, turn);
-    if (checkForWin(board, turn) == 1)
-        goto end;
+    for (int turn = 1; turn <= 9; turn ++) {
 
+        int playerNum;
+        if (turn % 2 == 0)
+            playerNum = 2;
+        else
+            playerNum = 1;
+
+        play(board, playerNum);
+        if (checkForWin(board, playerNum) == 1)
+            goto end;
+    }
     end:
         drawBoard(board);
         return 0;
 }
 
-int drawBoard(char array[])
-{
-    printf("%s",array);
-}
-
-int checkForWin(char array[], int turn){
-    if (array[0] == array[2] && array[0] == array[4]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[12] == array[14] && array[12] == array[16]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[24] == array[26] && array[24] == array[28]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[0] == array[12] && array[0] == array[24]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[2] == array[14] && array[2] == array[26]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[4] == array[16] && array[4] == array[28]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[0] == array[14] && array[0] == array[28]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-    if (array[4] == array[14] && array[4] == array[24]){
-        printf("\nPlayer %d Wins!!\n", turn);
-        return 1;
-    }
-}
-
-int markbaord(char array[],int turn)
-{
-    int move;
-    printf("Player %d type a location:\n", turn);
-    scanf("%d",&move);
-    if (move == 1)
-        move = 0;
-    if (move == 2)
-        move = 2;
-    if (move == 3)
-        move = 29;
-    if (move == 4)
-        move = 12;
-    if (move == 5)
-        move = 14;
-    if (move == 6)
-        move = 16;
-    if (move == 7)
-        move = 24;
-    if (move == 8)
-        move = 26;
-    if (move == 9)
-        move = 28;
-    if (move == 29)
-        move = 4;
-    if (turn == 1)
-        array[move] = 'X';
-    if (turn == 2)
-        array[move] = 'O';
-}
